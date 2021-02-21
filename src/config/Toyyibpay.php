19740449c1ca0536a6b5c9f2524e687f344eb852
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2021-02-21 14:25:54 +0800

    change by default using sandbox mode

--- src/config/Toyyibpay.php
+++ src/config/Toyyibpay.php
@@ -4,6 +4,6 @@ return [
 
   'client_secret' => env('TOYYIBPAY_USER_SECRET_KEY', ''),
   'redirect_uri' => env('TOYYIBPAY_REDIRECT_URI', ''),
-  'sandbox' => env('TOYYIBPAY_SANDBOX', false),
+  'sandbox' => env('TOYYIBPAY_SANDBOX', true),
 
 ];
