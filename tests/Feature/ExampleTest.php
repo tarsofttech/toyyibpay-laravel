8a763cd7037309b70eb54f408196e2c1fd360617
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2021-07-10 15:17:12 +0800

    adding example test case

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tarsoft\Toyyibpay\Tests\Feature;
+
+use Tarsoft\Toyyibpay\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
